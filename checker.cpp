25e3c439d7bfa89302597d2fb011989284e629b9
Ajay Kumar <41167768+ajay300898@users.noreply.github.com>
2020-08-28 16:32:26 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,5 +1,6 @@
 #include <assert.h>
-
+#include <iostream>
+using namespace std;
 // enum Range
 bool isBPOk(float bpm);
 bool isSpoOk(float spo2);
@@ -8,34 +9,82 @@ bool isVitalOk(float value, float low, float high);
 const int bp_limits[] = {70,150};
 const int resp_limits[] = {30,95};
 
-bool isVitalOk(float value, float low, float high){
-  return (value >= low && value <= high);
-}
-bool isBPOk(float bpm){
-  return isVitalOk(bpm,bp_limits[0],bp_limits[1]);
-}
+class Alert{
+  public:
+  virtual void raiseAnAlert(const char* vital, const char *message) = 0;
+  
+};
 
-bool isSpoOk(float spo2){
-  return isVitalOk(spo2,90,100);
-}
+class AlertInSMS : public Alert{
+  public:
+  virtual void raiseAnAlert(const char* vital, const char *message) override{
+    cout << "send SMS :" << endl;
+  }
+};
 
-bool isRespOk(float respRate){
-  return isVitalOk(respRate,resp_limits[0],resp_limits[1]);
-}
+class AlertInSoung : public Alert{
+  public:
+  virtual void raiseAnAlert(const char* vital, const char *message) override{
+    cout << "Make Alert sound :" << endl;
+  }
+};
+class CheckVitals{
+  private:
+    Alert* alert;
+  public:
+  CheckVitals(): alert(0){}
+  void setAlert(Alert* alert_requested){
+    alert = alert_requested;
+  }
+  
+  bool isVitalOk(float value, float low, float high){
+    return (value >= low && value <= high);
+  }
+  bool isBPOk(float bpm){
+    if(isVitalOk(bpm,bp_limits[0],bp_limits[1])){
+      return true;
+    }
+    alert->raiseAnAlert(bpm,"BP ");
+    return false;
+  }
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return isBPOk(bpm) && isSpoOk(spo2) && isRespOk(respRate);
-}
+  bool isSpoOk(float spo2){
+    if(isVitalOk(spo2,90,100)){
+      return true;
+    }
+    alert->raiseAnAlert(spo2,"SPO2");
+    return false;
+  }
+
+  bool isRespOk(float respRate){
+    if(isVitalOk(respRate,resp_limits[0],resp_limits[1])){
+      return true;
+    }
+    alert->raiseAnAlert(respRate,"RESP");
+    return false;
+  }
+
+  bool vitalsAreOk(float bpm, float spo2, float respRate) {
+    bool res1,res2,res3;
+    res1 = isBPOk(bpm);
+    res2 = isSpoOk(spo2);
+    res3 = isRespOk(respRate);
+    return res1 && res2 && res3;
+  }
+};
 
 int main() {
-  assert(isVitalOk(10,50,100) == false);
-  assert(isVitalOk(1000,50,100) == false);
-  assert(isVitalOk(70,50,100) == true);
-  assert(vitalsAreOk(80, 95, 60) == true);
-  assert(vitalsAreOk(60, 90, 40) == false);  // test bp low
-  assert(vitalsAreOk(160, 90, 40) == false); // test bp high
-  assert(vitalsAreOk(80, 99, 25) == false);  // test resp low
-  assert(vitalsAreOk(80, 99, 100) == false); // test resp low
-  assert(vitalsAreOk(80, 85, 60) == false); // test spo2 low
+  CheckVitals x;
+  x->setAlert(new AlertInSMS);
+  
+  assert(x->isVitalOk(10,50,100) == false);
+  assert(x->isVitalOk(1000,50,100) == false);
+  assert(x->isVitalOk(70,50,100) == true);
+  assert(x->vitalsAreOk(80, 95, 60) == true);
+  assert(x->vitalsAreOk(60, 90, 40) == false);  // test bp low
+  assert(x->vitalsAreOk(160, 90, 40) == false); // test bp high
+  assert(x->vitalsAreOk(80, 99, 25) == false);  // test resp low
+  assert(x->vitalsAreOk(80, 99, 100) == false); // test resp low
+  assert(x->vitalsAreOk(80, 85, 60) == false); // test spo2 low
 
 }
